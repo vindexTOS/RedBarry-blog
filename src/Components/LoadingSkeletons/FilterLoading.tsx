2fb1736f50aeba8e95b6a71f4b313def57144b59
vindexTOS <giorgikutateladze1998@gmail.com>
2023-12-26 15:26:43 +0400

    re design loading skeleton for filter

--- src/Components/LoadingSkeletons/FilterLoading.tsx
+++ src/Components/LoadingSkeletons/FilterLoading.tsx
@@ -7,40 +7,19 @@ export default function FilterLoading() {
         className={`h-[30px] w-[60px]  bg-gray-300  rounded-[12px]   py-[5px] px-[12px]   `}
       ></div>
       <div
-        className={`h-[30px] w-[100px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
+        className={`h-[30px] w-[160px] bg-gray-300 py-[5px] px-[12px] rounded-[16px]  `}
       ></div>
       <div
-        className={`h-[30px] w-[60px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
+        className={`h-[30px] w-[60px] bg-gray-300 py-[5px] px-[12px] rounded-[16px]  `}
       ></div>
       <div
-        className={`h-[30px] w-[80px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
+        className={`h-[30px] w-[80px] bg-gray-300 py-[5px] px-[12px] rounded-[16px]  `}
       ></div>
       <div
-        className={`h-[30px] w-[170px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
+        className={`h-[30px] w-[80px] bg-gray-300 py-[5px] px-[12px] rounded-[16px]  `}
       ></div>
       <div
-        className={`h-[30px] w-[80px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
-      ></div>
-      <div
-        className={`h-[30px] w-[80px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
-      ></div>
-      <div
-        className={`h-[30px] w-[180px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
-      ></div>
-      <div
-        className={`h-[30px] w-[50px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
-      ></div>
-      <div
-        className={`h-[30px] w-[120px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
-      ></div>
-      <div
-        className={`h-[30px] w-[90px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
-      ></div>
-      <div
-        className={`h-[30px] w-[60px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
-      ></div>{" "}
-      <div
-        className={`h-[30px] w-[80px] bg-gray-300 py-[5px] px-[12px] rounded-[12px]  `}
+        className={`h-[30px] w-[160px] bg-gray-300 py-[5px] px-[12px] rounded-[16px]  `}
       ></div>
     </div>
   );
